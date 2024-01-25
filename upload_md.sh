cdbd6544b29d3a282f973b02bf0238853a41d576
Rediet <rediet@mosip.io>
2024-01-25 07:16:34 +0530

    minor fix

--- upload_md.sh
+++ upload_md.sh
@@ -9,8 +9,8 @@ fi
 read -p "Enter IAM username: " iam_user
 
 # This username is hardcoded in sql scripts
-DB_PWD=T68sh2vLf2
-DB_HOST= postgres.dst-training.mosip.net
+DB_PWD='T68sh2vLf2'
+DB_HOST= 'postgres.dst-training.mosip.net'
 DB_PORT=5432
 XLS=mosip_master/xlsx
 
